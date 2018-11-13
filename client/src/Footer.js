240625681b7e35959b107ba11eaf2dff8a3a7302
Fausto Rendon <faustor001@gmail.com>
2018-11-12 22:22:22 -0800

    Update footer links

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
         <h2 className="major">Get in touch</h2>
         <p>I am currently looking for opportunities in Software Engineering or Full Stack Development. I am also interested in collaborating in open source projects.</p>
         <ul className="contact">
-          <li className="fa-envelope"><a href="#">information@untitled.tld</a></li>
-          <li className="fa-github"><a href="#">twitter.com/untitled-tld</a></li>
-          <li className="fa-linkedin"><a href="#">facebook.com/untitled-tld</a></li>
+          <li className="fa-envelope"><a href="mailto:contact@faustorendon.com">contact@faustorendon.com</a></li>
+          <li className="fa-github"><a href="https://github.com/frendon001">github.com/frendon001</a></li>
+          <li className="fa-linkedin"><a href="https://www.linkedin.com/in/faustorendon/">linkedin.com/in/faustorendon/</a></li>
         </ul>
         <ul className="copyright">
           <li>&copy; All rights reserved.</li><li><small>Design: <a href="http://html5up.net">HTML5 UP</a></small></li>
