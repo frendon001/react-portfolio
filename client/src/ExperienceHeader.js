c225724452acd7e96090c13dd073b341af952375
Fausto Rendon <faustor001@gmail.com>
2018-11-13 19:42:52 -0800

    Update experience header section

--- client/src/ExperienceHeader.js
+++ client/src/ExperienceHeader.js
@@ -3,19 +3,23 @@ import ExperienceSkills from './ExperienceSkills';
 
 function ExperienceHeader(props) {
   return (
-    <div className="content">
-      <div className="exp-group">
-        <h3>Current Location:</h3>
-        <p>{props.about.location}</p>
-      </div>
-      <div className="exp-group">
-        <h3>Education:</h3>
-        <p>{props.about.education}</p>
-      </div>
-      <div className="exp-group">
-        <h3>Tehnical Skills:</h3>
-        <ExperienceSkills skills={props.about.skills} keyname="exp_header"/>
-      </div>
+    <div className="content aboutme-grid-wrapper">
+    <section className="about-sidebar">
+      <h2 className="major">About Me</h2>
+      <p>{props.about.aboutme}</p>
+    </section>
+    <div className="exp-group about-location">
+      <h3>Current Location:</h3>
+      <p>{props.about.location}</p>
+    </div>
+    <div className="exp-group about-education">
+      <h3>Education:</h3>
+      <p>{props.about.education}</p>
+    </div>
+    <div className="exp-group about-skills">
+      <h3>Tehnical Skills:</h3>
+      <ExperienceSkills skills={props.about.skills} keyname="exp_header"/>
+    </div>
     </div>
   );
 }
