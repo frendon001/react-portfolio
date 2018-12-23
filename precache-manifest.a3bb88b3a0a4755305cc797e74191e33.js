72976872aca0a2baa62b4d1e16ed11e4ee52290d
Fausto Rendon <faustor001@gmail.com>
2018-12-23 12:57:49 -0800

    Updates

---
+++ precache-manifest.a3bb88b3a0a4755305cc797e74191e33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "593b4efba6f29b1e552f",
+    "url": "/static/js/main.593b4efb.chunk.js"
+  },
+  {
+    "revision": "32c1aa33c2be79598dab",
+    "url": "/static/js/1.32c1aa33.chunk.js"
+  },
+  {
+    "revision": "593b4efba6f29b1e552f",
+    "url": "/static/css/main.b9bf0043.chunk.css"
+  },
+  {
+    "revision": "c09122bff4f2ce683b279f0f148bbfa5",
+    "url": "/index.html"
+  }
+];
