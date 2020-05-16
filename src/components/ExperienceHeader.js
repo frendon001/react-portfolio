b0a0d2935d331bdabf3a6589d374acd1df1fba08
Fausto Rendon <contact@faustorendon.com>
2020-05-16 12:21:50 -0700

    Add banana budget and remove crypto app

--- src/components/ExperienceHeader.js
+++ src/components/ExperienceHeader.js
@@ -6,7 +6,7 @@ function ExperienceHeader(props) {
     <div className="content aboutme-grid-wrapper">
     <section className="about-sidebar">
       <h2 className="major">About Me</h2>
-      <p>{props.about.aboutme}</p>
+      <p>{props.about.aboutMe}</p>
     </section>
     <div className="exp-group about-location">
       <h3>Current Location:</h3>
@@ -17,7 +17,7 @@ function ExperienceHeader(props) {
       <p>{props.about.education}</p>
     </div>
     <div className="exp-group about-skills">
-      <h3>Tehnical Skills:</h3>
+      <h3>Technical Skills:</h3>
       <Skills skills={props.about.skills} keyname="exp_header"/>
     </div>
     </div>
